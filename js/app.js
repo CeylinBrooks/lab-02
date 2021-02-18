6a2653ca1e3ef655741928cc1a1e9b340b796cc6
ceylinbrooks <ceylinbrooks@gmail.com>
2021-02-17 19:43:45 -0500

    updating js

--- js/app.js
+++ js/app.js
@@ -11,31 +11,44 @@ function Horns(image_url, title, description, keyword, horns) {
 Horns.allHorns = [];
 
 Horns.prototype.renderHorns = function() {
+  console.log('test');
   const $liCopy = $('li:first-child').clone();
   $liCopy.find('h2').text(this.title);
   $liCopy.find('p').text(this.description);
   $liCopy.find('img').attr('src', this.image_url);
   console.log(this);
-  $('ul').append($liCopy);
+  $('#show-pics').append($liCopy);
+  
 };
 
-$.ajax('data/page-1.json').then(bringTheHorns);
 
 
-
+$.ajax('../data/page-1.json').then(bringTheHorns);
+const uniArr = [];
 function bringTheHorns(value){
   console.log(value);
 
-bringTheHorns.forEach(hornedObj => {
-  new Horns(hornedObj.image_url,hornedObj.title, hornedObj.description,hornedObj.keyword,hornedObj.horns)
+  value.forEach(hornedObj => {
+    new Horns(hornedObj.image_url,hornedObj.title, hornedObj.description,hornedObj.keyword,hornedObj.horns)
+    uniArr.push(hornedObj.keyword);
+    
+  }
 });
 
-Horns.allHorns,forEach(Horns => Horns.renderHorns());
+  Horns.allHorns.forEach(Horns => Horns.renderHorns());
+}
+
+// $("li")
+//   .filter(function(keyword){
+//     return $("",this).length === 
+//   })
 
 $('select').on('click', handleClick);
 $('select').on('click', handleClick);
 
-function handleClick(){
+function handleClick() {
   $('li').hide();
   $('li: contains()').show();
 }
+
+
