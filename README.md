92ddf5e47af2cc80d85909af29002c6572f221a1
FIZZO PANNOSCH <fizzo999@gmail.com>
2021-02-18 20:03:17 -0800

    final push

--- README.md
+++ README.md
@@ -1,33 +1,52 @@
 # lab-02
 
-A Gallery of Horns is a interactive sorting website based on how many horms they have!
+A Gallery of Horns is an interactive sorting website based on how many horns they have!
+School project for Codefellows 301 - display first set of pictures (ajax.jquery call) - using jQuery DOM method - populate dropdown menu with available options
+display a second set of ajax.jquery pictures - add buttons to allow for pagination - add 2 more buttons to sort pictures by horns or by name
 
 ## Pair Programming Project Feb 16 2021
 Ceylin Brooks
 Fizzo Pannosch
 
+special thanks to TAs
+Chance Harmon
+Sara Russert
+Skyler Burger
+Ron Dunphy
+Michael Eclavea
+Alistair Blake
+and instructor Nicholas Carignan
+
+
 ### Time sheet
 ### Number and name of feature: Display images
 
-Feature #1: Display images ________________________________
++ Feature #1: Display images Feb16 2021
++ Estimate of time needed to complete: 1:00 Hour 
++ Start time: 3:00 PST 
++ Finish time: 4:00 PST
++ Actual time needed to complete: 1:00 HOUR 
 
-Estimate of time needed to complete: 1:00 Hour _____
+### Number and name of feature: drop down menu
 
-Start time: 3:00 PST _____
-
-Finish time: 4:00 PST_____
-
-Actual time needed to complete: 1:00 HOUR _____
++ Feature #1: Display images Feb16 2021
++ Estimate of time needed to complete: 2:00 Hour 
++ Start time: 4:00 PST 
++ Finish time: 10:00 PST
++ Actual time needed to complete: 6:00 HOUR 
 
 ### Number and name of feature: Pagination
 
-Feature #1: Pagination________________________________
++ Feature #1: Pagination #1 Feb17 2021
++ Estimate of time needed to complete: 1:00 Hour 
++ Start time: 2:45 PST 
++ Finish time: 10:00 PST
++ Actual time needed to complete: 7 HOURs 
 
-Estimate of time needed to complete: 1:00 Hour _____
+### Number and name of feature: Pagination #2
 
-Start time: 2:45 PST _____
-
-Finish time:  PST_____
-
-Actual time needed to complete:  HOUR _____
-
++ Feature #1: Pagination #2 and de bugging Feb18 2021
++ Estimate of time needed to complete: 4:00 Hour 
++ Start time: 3:00 PST 
++ Finish time: 7:00 PST
++ Actual time needed to complete: 4 HOURs 
--- js/app.js
+++ js/app.js
@@ -10,26 +10,29 @@ function UnicornPic(image_url, title, description, keyword, horns) {
 }
 UnicornPic.allPics = [];
 
+// vanilla JS: create everything from scratch then append it: 12 lines of code
+// jQuery: copy everything and change each element line by line: 6 lines of code
+// Mustache: copy everything and let Mustache change each element for you: 3 lines of code
+
+// the jQuerry method - to first populate the page on load
 UnicornPic.prototype.renderUnicornPic = function () {
   const $liCopy = $('li:first-child').clone();
   $liCopy.addClass(this.keyword);
   $liCopy.find('h2').text(this.title);
   $liCopy.find('img').attr('src', this.image_url);
   $liCopy.find('p').text(this.description);
-  // console.log(this);
-  $('main>ul').append($liCopy);
+  $('#thejQuerryUl').append($liCopy);
 };
 
+// the mustache method to refresh the page click button one or two
 UnicornPic.prototype.renderWithJQueryAndMustache = function () {
-  // 1. get the html from the script tag
+  // 1. get the html from the script tag - and NO UL's !!!
   // 2. pass the html and an object to Mustache.render(html, object)
   // 3. append it to the page
   const $templateFromHtml = $('#mustache-template-tr').html();
   const outputFromMustache = Mustache.render($templateFromHtml, this);
-  $('main > ul').append(outputFromMustache);
-  // css for a table that is a direct child of the body
+  $('#theMustacheId').append(outputFromMustache);
 };
-
 
 // $('mustache-template-tr').remove();
 // $('mustache-template-tr').empty();
@@ -43,6 +46,9 @@ $.ajax('../data/page-1.json').then(data => {
 
   $.ajax('../data/page-2.json').then(data => {
     resurrectDataFunction(data, UnicornPic.newUnicornArray2);
+    UnicornPic.allPics.forEach(unicornObject => {
+      unicornObject.renderUnicornPic();
+    });
     dropDownMenu();
   });
 });
@@ -56,7 +62,7 @@ function resurrectDataFunction(resurrectedData, nameOfTheArray) {
 function dropDownMenu() {
   const tempArr = [];
   UnicornPic.allPics.forEach(unicornObject => {
-    unicornObject.renderUnicornPic();
+    // unicornObject.renderUnicornPic();
     if (!tempArr.includes(`${unicornObject.keyword.toLowerCase()}`)) {
       tempArr.push((unicornObject.keyword).toLowerCase());
     }
@@ -74,7 +80,6 @@ function dropDownMenu() {
   // here we want to do another check to see if the li's on the page actually have that keyword - then include the keyword in the array - otherwise remove that keyword from the array;
   tempArr.forEach(label => $('select').append(`<option value="${label}">${label}</option>`));
 }
-
 // need event handler that listens for 'change' (behaves like a checkbox behind the scenes) - on('change') listen to some value of keyword .... once you can get that value (console.log) -   $('li').hide(); - hide everything and show only selection keyword objects - loop through all objects in allPics - if it has that value on it
 //  write it into a function
 
@@ -82,74 +87,79 @@ $('select').on('change', handleClickingDropDown);
 
 function handleClickingDropDown(event) {
   let eventValue = event.target.value;
-  // Hide and show way: hide all and then show some : HTML focused
   $('li').hide();
   // could still change this to a forEach method ....
+
   for (let i = 0; i < UnicornPic.allPics.length; i++) {
     if (UnicornPic.allPics[i].keyword === eventValue) {
-      // $('li:includes=`${eventValue}`').show();
       $(`li[class="${eventValue}"]`).show();
     }
   }
 }
+// this function empties out both the js and the mustach rendered pictures
+function emptyOutPage() {
+  $('#thejQuerryUl').empty();
+  $('#theMustacheId').empty();
+}
+// this sorting function takes in the array(either 1 or 2) and the sort criterion to sort by
+function sortBy(arrayPassedInHere, sortingParam) {
+  arrayPassedInHere.sort(function (left, right) {
+    if (left[sortingParam] > right[sortingParam]) {
+      return 1;
+    } else if (left[sortingParam] < right[sortingParam]) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+  return arrayPassedInHere;
+}
 
-// vanilla: create everything from scratch then append it: 12 lines of code
-// jQuery: copy everything and change each element line by line: 6 lines of code
-// Mustache: copy everything and let Mustache change each element for you: 3 lines of code
-let thePageYouAreOn = 1;
+
+let thePageYouAreOn = 0;
 
 // Pagination
 $('button:first-of-type').on('click', () => {
+  emptyOutPage();
   thePageYouAreOn = 1;
-  // $('body > table tr:nth-child(n+2)').empty();
-  $('ul').empty();
-  $('ul').append('<li><h2></h2><img src="" alt=""><p></p></li>');
+  // $('ul').append('<li><h2></h2><img src="" alt=""><p></p></li>');
   UnicornPic.newUnicornArray.forEach(unicornObject => unicornObject.renderWithJQueryAndMustache());
-  // UnicornPic.newUnicornArray.forEach(unicornObject => unicornObject.renderUnicornPic());
   // another idea - empty out the drop down menu and rerender it;
-  // console.log(UnicornPic.newUnicornArray);
 });
 
 $('button:nth-of-type(2)').on('click', () => {
+  emptyOutPage();
   thePageYouAreOn = 2;
-  // $('body > table tr:nth-child(n+2)').empty();
-  $('ul').empty();
-  $('ul').append('<li><h2></h2><img src="" alt=""><p></p></li>');
-
   UnicornPic.newUnicornArray2.forEach(unicornObject => unicornObject.renderWithJQueryAndMustache());
-  // UnicornPic.newUnicornArray2.forEach(unicornObject => unicornObject.renderUnicornPic());
+  // again empty out dropdown menu and rerender it
 });
 
 $('button:nth-of-type(3)').on('click', () => {
-  // console.log(thePageYouAreOn);
-  // $('body > table tr:nth-child(n+2)').empty();
-  $('ul').empty();
-
+  emptyOutPage();
   // sort images by name
-  $('ul').append('<li><h2></h2><img src="" alt=""><p></p></li>');
-  // Pet.allPets2.forEach(pet => pet.renderWithJQueryAndMustache());
   if (thePageYouAreOn === 1) {
-    UnicornPic.newUnicornArray.forEach(unicornObject => unicornObject.renderUnicornPic());
-    console.log(thePageYouAreOn);
+    // console.log('before SORT', UnicornPic.newUnicornArray);
+    let newlySortedArray = sortBy(UnicornPic.newUnicornArray, 'title');
+    // console.log('after SORT', UnicornPic.newUnicornArray);
+    newlySortedArray.forEach(unicornObject => unicornObject.renderWithJQueryAndMustache());
   } else {
-    UnicornPic.newUnicornArray2.forEach(unicornObject => unicornObject.renderUnicornPic());
+    // console.log('before SORT 2', UnicornPic.newUnicornArray);
+    let newlySortedArray2 = sortBy(UnicornPic.newUnicornArray2, 'title');
+    // console.log('after SORT 2', UnicornPic.newUnicornArray);
+    newlySortedArray2.forEach(unicornObject => unicornObject.renderWithJQueryAndMustache());
   }
-
-
 });
 
 $('button:nth-of-type(4)').on('click', () => {
-  // console.log(thePageYouAreOn);
-  // $('body > table tr:nth-child(n+2)').empty();
-  $('ul').empty();
-
+  emptyOutPage();
   // sort images by horns
-  $('ul').append('<li><h2></h2><img src="" alt=""><p></p></li>');
-  // Pet.allPets2.forEach(pet => pet.renderWithJQueryAndMustache());
   if (thePageYouAreOn === 1) {
-    UnicornPic.newUnicornArray.forEach(unicornObject => unicornObject.renderUnicornPic());
+    let newlySortedArray = sortBy(UnicornPic.newUnicornArray, 'horns');
+    newlySortedArray.forEach(unicornObject => unicornObject.renderWithJQueryAndMustache());
   } else {
-    UnicornPic.newUnicornArray2.forEach(unicornObject => unicornObject.renderUnicornPic());
+    let newlySortedArray2 = sortBy(UnicornPic.newUnicornArray2, 'horns');
+    newlySortedArray2.forEach(unicornObject => unicornObject.renderWithJQueryAndMustache());
   }
 });
+
 
